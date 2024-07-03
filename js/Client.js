0679d376fdc27d77122252779a48d6296f486972
Gowtham2721 <69364196+Gowtham2721@users.noreply.github.com>
2024-07-03 08:56:57 +0530

    Update Client.js

--- js/Client.js
+++ js/Client.js
@@ -5,6 +5,11 @@ document.getElementById('detailsForm1').addEventListener('submit', function(even
     window.open('https://docs.google.com/spreadsheets/d/1EF9FPqrc6xPEPEbUbLZYKRWNYfbvTQ87_piHixYzG9c/edit?usp=sharing', '_blank');
 });
 
+document.getElementById('detailsForm2').addEventListener('submit', function(event) {
+    event.preventDefault();
+    window.open('https://docs.google.com/spreadsheets/d/1bQD48RHKhSG-iwRZOJUrvKvZo0phD7Tu5ItsjwFQbh4/edit?usp=sharing', '_blank');
+});
+
 document.getElementById('amountForm').addEventListener('submit', function(event) {
     event.preventDefault();
     window.open('https://drive.google.com/file/d/18w0wyAo9za6ngKQHNcDOh6g0Hb4_80Gh/view?usp=drivesdk', '_blank');
